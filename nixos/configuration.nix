28a76096e230e02dd2221b1d2a331ced19a81eb2
Zhansong Li <lizhansong@hvariant.com>
2019-10-28 21:37:14 +1100

    install ibus

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,11 @@
   i18n = {
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
+
+    inputMethod.enabled = "ibus";
+    inputMethod.ibus.engines = with pkgs.ibus-engines; [
+      libpinyin
+    ];
   };
 
   time.timeZone = "Australia/Sydney";
